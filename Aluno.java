4bff9eb634564c8362e0d034022e3ec67742493d
CajuBarela <maria.netto01@etec.sp.gov.br>
2023-10-25 16:59:04 +0000

    S

---
+++ Aluno.java
@@ -0,0 +1,68 @@
+public class Aluno{
+    private String nome;
+    private double media;
+    private int numFaltas;
+    private int numAulas;
+
+    //fazer o get e o set, além do método construtor padrão
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public int getNumFaltas() {
+        return numFaltas;
+    }
+
+    public void setNumFaltas(int numFaltas) {
+        this.numFaltas = numFaltas;
+    }
+
+    public int getNumAulas() {
+        return numAulas;
+    }
+
+    public void setNumAulas(int numAulas) {
+        this.numAulas = numAulas;
+    }
+    
+    public Aluno(){
+
+    }
+
+    public void Aluno(String nome){
+        this.nome = nome;
+
+    }
+
+    public void Aluno(String nome, double media){
+        this.nome = nome;
+        this.media = media;
+    }
+
+    public void Aluno(String nome, double media, int numFaltas){
+        this.nome = nome;
+        this.media = media;
+        this.numFaltas = numFaltas;
+    }
+
+    public void Aluno(String nome, double media, int numFaltas, int numAulas){
+        this.nome = nome;
+        this.media = media;
+        this.numFaltas = numFaltas;
+        this.numAulas = numAulas;
+    }
+
+}
